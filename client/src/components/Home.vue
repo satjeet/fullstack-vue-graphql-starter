260e6e3f274cd975d232c26891bc350f59286cc4
Arturo <arturo.alfaro87@gmail.com>
2019-05-17 00:35:41 -0400

    antes de comenzar con vuex

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,90 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <v-btn color="primario"> Boton</v-btn>
+    <div v-if="$apollo.loading">Loading...</div>
+    <ul
+      v-else
+      v-for="post in getPosts"
+      :key="post._id"
+    >
+      <li>
+        {{post.title}}
+        {{post.image_Url}}
+        {{post.description}}
+
+      </li>
+      <li>{{post.likes}}</li>
+
+    </ul>
+    <v-container
+      text-xs-center
+      v-if="getPosts"
+    >
+      <v-flex xs12>
+        <v-carousel
+          v-bind="{ 'cycle': true }"
+          interval="3000"
+        >
+          <v-carousel-item
+            v-for="post in getPosts"
+            :key="post._id"
+            :src="post.imageUrl"
+          >
+            <h1 id="carousel__title">{{post.title}}</h1>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-container>
+
   </v-container>
 
 </template>
 
 <script>
+import { gql } from "apollo-boost";
+
 export default {
-  name: "home"
+  name: "home",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `,
+      result({ data, loading }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+        }
+      }
+    }
+  }
 };
 </script>
+
+
+
+<style>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,25 @@
-import '@babel/polyfill'
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import "@babel/polyfill";
+import Vue from "vue";
+import "./plugins/vuetify";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+Vue.use(VueApollo);
 
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
+
+Vue.config.productionTip = false;
+// provide: apolloProvider.provide(),
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
